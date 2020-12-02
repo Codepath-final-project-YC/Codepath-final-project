c1fd8ad7d8463a771aab16ff83931f1898882795
yike <yikel@seas.upenn.edu>
2020-12-01 23:33:25 -0500

    New default branch

--- HabitTracker/View Controllers/HomeViewController.swift
+++ HabitTracker/View Controllers/HomeViewController.swift
@@ -19,13 +19,38 @@ class HomeViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToCALayerScene" {
-            guard let vc = segue.destination as? CALayerViewController else { return }
-            
+            guard let vc1 = segue.destination as? CALayerViewController else { return }
+
+        }
+        if segue.identifier == "goToComposeVC" {
+            guard let vc2 = segue.destination as? ComposeViewController else { return }
+
+        }
+        
+        if segue.identifier == "goToTrackHabitVC" {
+            guard let vc3 = segue.destination as? TrackHabitViewController else { return }
+
+        }
+        if segue.identifier == "goToAnalyzeHabit" {
+            guard let vc4 = segue.destination as? AnalyzeHabitViewController else { return }
+
         }
     }
 
 
     @IBAction func drawCate1Button(_ sender: Any) {
         performSegue(withIdentifier: "goToCALayerScene", sender: self)
+    }
+    
+    @IBAction func ToStartHabitButton(_ sender: Any) {
+        performSegue(withIdentifier: "goToComposeVC", sender: self)
+    }
+    
+    @IBAction func trackHabitButton(_ sender: Any) {
+        performSegue(withIdentifier: "goToTrackHabitVC", sender: self)
+
+    }
+    @IBAction func AnalyzeHabitButton(_ sender: Any) {
+        performSegue(withIdentifier: "goToAnalyzeHabit", sender: self)
     }
 }
